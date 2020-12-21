b56c98e25b63c3374337418b2ef886ced29fa237
AJ Friend <ajfriend@gmail.com>
2020-12-20 18:27:23 -0800

    little cleaner now

--- notebooks/try4/util2d.py
+++ notebooks/try4/util2d.py
@@ -1,34 +1,4 @@
-import matplotlib.pyplot as plt
-import toolz
-import pandas as pd
-
-import matplotlib.pyplot as plt
-
-
-def get_scaled_cmap(cmap='YlOrRd', lower=0.0, upper=1.0):
-
-    if isinstance(cmap, str):
-        cmap = plt.get_cmap(cmap)
-
-    if lower == upper:
-        pass # what to do!
-
-    def foo(value):
-        assert lower <= value <= upper
-
-        if lower == upper:
-            value = 1.0
-        else:
-            value = (value - lower)/float(upper - lower)
-
-        out = cmap(value, bytes=True)
-        out = map(int, out)
-
-        r, g, b, a = out
-
-        return r,g,b,a
-
-    return foo
+from util import get_scaled_cmap
 
 
 def make_pdk_rows(
