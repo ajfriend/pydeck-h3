5cffbe5a9bab775d062e3e1f4cc66442eeb3ca22
AJ Friend <ajfriend@gmail.com>
2020-12-26 17:26:42 -0500

    working

--- src/pydeck_h3/hexcluster.py
+++ src/pydeck_h3/hexcluster.py
@@ -1,38 +1,66 @@
 import pydeck as pdk
-import pandas as pd
+
 
 MB_KEY = 'pk.eyJ1IjoiYWpmcmllbmQiLCJhIjoiY2pmbmRjczJmMTVkMzJxcW92Y2E4cHZjdCJ9.Jf-gFXU7FOIQxALzPajbdg'
 
 
-# Define a layer to display on a map
-layer = pdk.Layer(
-    "H3ClusterLayer",
+import toolz
+
+from .pdk_rows.util import transpose
+from .pdk_rows.helpers import color_helper
+
+
+def make_pdk_rows(
     rows,
-    pickable=True,
-   stroked=True,
-    filled=True,
-    extruded = False,
-    get_hexagons = 'hexset',
-    get_fill_color = 'color',
-    get_line_color = [0, 0, 0],
-    get_line_width = 100,
-    get_elevation = 'value',
-#    line_width_min_pixels=2,
-    opacity=.7,
-    elevation_scale = 5,
-    wireframe=True,
-)
-
-# Set the viewport location
-view_state = pdk.ViewState(latitude=37.7749295, longitude=-122.4194155, zoom=9)
+    color,
+    cmap = 'YlOrRd', #string or callable, but this function doesn't check
+):
+    cols = [
+        color_helper(rows, color, cmap=cmap),
+    ]
 
 
-# Render
-r = pdk.Deck(layers=[layer], initial_view_state=view_state,     mapbox_key = MB_KEY,
-    map_style = 'mapbox://styles/mapbox/light-v10',)
-r.to_html()
+    cols = toolz.merge(*cols)
+    _rows = transpose(cols)
+
+    pdk_rows = [
+        {
+            **row,
+            **_row
+        }
+        for row, _row in zip(rows, _rows)
+    ]
+
+    return pdk_rows
 
 
 
-def plot(data, col_hex='hexset', col_color, cmap):
-    pass
+def plot(data, col_hex='hexset', col_color='color', cmap='YlOrRd', line_width=100, opacity=0.7):
+
+    data = make_pdk_rows(data, col_color, cmap=cmap)
+
+    layer = pdk.Layer(
+        'H3ClusterLayer',
+        data,
+        pickable = True,
+        stroked = True,
+        filled = True,
+        extruded = False,
+        get_hexagons = col_hex,
+        get_fill_color = '_pdk_fill_color',
+        get_line_width = line_width,
+        opacity = opacity,
+    )
+
+
+    view_state = pdk.ViewState(latitude=37.7749295, longitude=-122.4194155, zoom=9)
+
+
+    deck = pdk.Deck(
+        layers=[layer],
+        initial_view_state=view_state,
+        mapbox_key = MB_KEY,
+        map_style = 'mapbox://styles/mapbox/light-v10',
+    )
+
+    return deck
