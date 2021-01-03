9a4dcd6a21600cd9d4eeea5b03d34a455c0ee0a5
AJ Friend <ajfriend@gmail.com>
2021-01-02 19:15:23 -0500

    plot3d for hexclusters

--- src/pydeck_h3/hexcluster.py
+++ src/pydeck_h3/hexcluster.py
@@ -80,9 +80,65 @@ def plot(data, col_hex='hexset', col_color='color', cmap='YlOrRd', line_width=10
         for row in data
     ])
 
+    #todo: can we improve on this? we don't need to send the whole list, probably just the min/max in lat/lng right?
     view = pdk.data_utils.compute_view(
         [h3.h3_to_geo(h)[::-1] for h in hexes]
     )
+
+
+    deck = pdk.Deck(
+        layers = [layer],
+        initial_view_state = view,
+        mapbox_key = MB_KEY,
+        map_style = 'mapbox://styles/mapbox/light-v10',
+    )
+
+    return deck
+
+
+def plot3d(data, col_hex='hexset', col_color='color', cmap='YlOrRd', opacity=0.7, elevation_scale=100):
+    """
+    data is a list of dictionaries.
+
+    !!! currently mutates input!
+    """
+
+    # just to make it so we don't mutate `data`
+    data = [
+        dict(row)
+        for row in data
+    ]
+
+    # sets don't parse to json, so make sure the hexset is a row
+    for row in data:
+        row[col_hex] = list(row[col_hex])
+
+    data = make_pdk_rows(data, col_color, cmap=cmap)
+
+    layer = pdk.Layer(
+        'H3ClusterLayer',
+        data,
+        pickable = True,
+        stroked = True,
+        filled = True,
+        extruded = True,
+        get_hexagons = col_hex,
+        get_fill_color = '_pdk_fill_color',
+        opacity = opacity,
+        elevation_scale = elevation_scale,
+        get_elevation = col_color,
+    )
+
+    hexes = set.union(*[
+        set(row[col_hex])
+        for row in data
+    ])
+
+    #todo: can we improve on this? we don't need to send the whole list, probably just the min/max in lat/lng right?
+    view = pdk.data_utils.compute_view(
+        [h3.h3_to_geo(h)[::-1] for h in hexes]
+    )
+    view.pitch = 45
 
 
     deck = pdk.Deck(
